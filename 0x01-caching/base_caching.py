1ceaa3656eb0e3294dd0b92bafdc892d378239b4
“Annah <njugunaannah@gmail.com>
2024-01-31 01:00:40 +0300

    edit

--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -25,8 +25,7 @@ class BaseCaching():
     def put(self, key, item):
         """ Add an item in the cache
         """
-        raise NotImplementedError("put must be
-                                  implemented in your cache class")
+        raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
         """ Get an item by key
